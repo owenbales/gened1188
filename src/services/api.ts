dd3fc2886a91513c2963b19fe799e80603cb9cb0
owenbales <owenbales011@gmail.com>
2025-04-29 02:52:17 -0400

    Update frontend to use Render backend URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://gened1188.onrender.com/api';
 
 export interface SearchParams {
   company: string;
